816f048c8b05b8db6913e083ecf952d21405ccff
Josh Cox <josh@transmute.network>
2018-07-13 19:33:37 -0500

    gh-pages

--- TimeOut.gd
+++
@@ -1,9 +0,0 @@
-extends Timer
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-func _on_Timer_timeout():
-	get_tree().change_scene("res://StartScreen.tscn")
